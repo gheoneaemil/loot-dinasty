6d1a5cde8a0005ff88b511d7924832aff9edbdb6
DIIMIIM <emilian@stater.co>
2025-02-11 23:38:42 +0200

    feat: main work flows almost done

--- contracts/LootKingdom.sol
+++ contracts/LootKingdom.sol
@@ -1,28 +1,130 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import "../deps/Ownable.sol";
+import "../deps/ReentrancyGuard.sol";
+import "../deps/IERC20.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Lock is Ownable, ReentrancyGuard {
+    event PackEditable(uint32 indexed packId);
+    event NewPack(uint32 indexed packId);
+    event HashKeyChangeFailed(address indexed user);
+    event OpenFulfillment(
+        address indexed user, 
+        uint32 indexed packId, 
+        uint256[] prizes, 
+        uint256[] prices,
+        uint256 randomness
+    );
 
-contract Lock {
-    // uint32 for pack id
-    // uint256[] is an array representing the smaller value of chance for each prize
-    // e.g: Item 1 10% chance, value 0 and Item 2 90% chance, value 10 
-    // + the last element of the array represinting the last max value of last item(Item 1), 99
-    mapping(uint32 => uint256[]) public packsToPrizes;
+    struct Pack {
+        address token;
 
-    // uint32 for pack id
-    // uint256 for the smallest value obtained from the packsToPrizes array
-    // uint256 for the amount/price of the prize
-    // address for the token address of the amount/price 
-    mapping(uint32 => mapping(uint256 => uint256)) packsToPrizeToPrice;
-    mapping(uint32 => mapping(uint256 => address)) packsToPrizeToToken;
+        // uint32 for pack id
+        // uint256[] is an array representing the smaller value of chance for each prize
+        // e.g: Item 1 10% chance, value 0 and Item 2 90% chance, value 10 
+        // + the last element of the array represinting the last max value of last item(Item 1), 99
+        uint256[] prizes;
 
+        uint256[] prices;
 
-    constructor() {
+        uint256 price;
+        uint256 allowedEditAccess;
     }
 
-    function rtp(uint32 packId) external view returns(uint8) {
+    struct Session {
+        string hashkey;
+        uint32 remaining;
+        uint256 forbiddenHashChangeAccess;
+    }
+
+    mapping(uint256 => Pack) public packs;
+    mapping(address => mapping(uint32 => Session)) public userToSession;
+
+    uint256 public editPeriod = 5 days;
+    uint256 public cooldownPeriod = 20 days;
+    uint256 public forbiddenHashChangeAccess = 20 minutes;
+    address public houseAddress;
+
+    constructor(address _houseAddress) Ownable(msg.sender) {
+        houseAddress = _houseAddress;
+    }
+
+    function setPack(
+        uint32 packId,
+        Pack calldata pack
+    ) external onlyOwner {
+        require(
+            pack.prizes.length <= 100 && 
+            pack.prizes.length == pack.prices.length,
+            "Invalid length"
+        );
+
+        if (packs[packId].prizes.length > 0) {
+            require(
+                packs[packId].allowedEditAccess >= block.timestamp,
+                "Cannot be edited right now"
+            );
+        }
+
+        packs[packId] = pack;
+        emit NewPack(packId);
+    }
+
+    function setAllowedEditAccess(
+        uint32 packId
+    ) external onlyOwner {
+        require(
+            packs[packId].allowedEditAccess + cooldownPeriod < block.timestamp,
+            "Already available"
+        );
+        packs[packId].allowedEditAccess = block.timestamp + editPeriod;
+        emit PackEditable(packId);
+    }
+
+    function open(
+        uint32 packId,
+        uint32 qty, 
+        string calldata hashkey
+    ) external payable nonReentrant {
+        Pack memory pack = packs[packId];
+        if (pack.token == address(0)) {
+            require(msg.value >= qty * pack.price, "Insufficient balance");
+            payable(houseAddress).transfer(msg.value);
+        } else {
+            IERC20 token = IERC20(pack.token);
+            uint256 balance = token.balanceOf(address(this));
+            require(balance >= qty * pack.price, "Insufficient balance");
+            token.transfer(houseAddress, balance);
+        }
+
+        Session memory session = userToSession[msg.sender][packId];
+        if (keccak256(abi.encodePacked(session.hashkey)) != keccak256(abi.encodePacked(hashkey))) {
+            if (session.forbiddenHashChangeAccess < block.timestamp) {
+                session.hashkey = hashkey;
+                session.forbiddenHashChangeAccess = block.timestamp + forbiddenHashChangeAccess;
+            } else {
+                emit HashKeyChangeFailed(msg.sender);
+            }
+        }
+        session.remaining += qty;
+    }
+
+    function fulfillUserOpen(
+        string calldata hashkey,
+        address user,
+        uint32 packId
+    ) external nonReentrant onlyOwner {
+        Session memory session = userToSession[user][packId];
+        require(session.remaining > 0, "No more attempts left");
+        Pack memory pack = packs[packId];
+        uint256 randomness = uint256(keccak256(abi.encodePacked(session.hashkey, hashkey))) % pack.prizes[pack.prizes.length-1];
+        --session.remaining;
+        emit OpenFulfillment(user, packId, pack.prizes, pack.prices, randomness);
+    }
+
+    function rtp(
+        uint32 packId
+    ) external view returns(uint8) {
         
     }
 }
