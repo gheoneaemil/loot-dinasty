9ca58b94755804f4cf5dc68d7f2d05e66a5b963e
DIIMIIM <emilian@stater.co>
2025-03-15 13:18:56 +0200

    feat: changed smart contract for supporting app using payment gateway

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # loot-dinasty
+##### https://sepolia.arbiscan.io/address/0x016921DC98d0be53A84d03e8AC304853Fa9F89D1
--- contracts/LootKingdom.sol
+++ contracts/LootKingdom.sol
@@ -1,50 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "../deps/Ownable.sol";
-import "../deps/IERC20.sol";
 
-error InvalidLength();
 error NotEditable();
-error CurrentlyUnchangeable();
-error InsufficientBalance();
 error Forbidden();
 
 contract LootKingdom is Ownable {
     event OpensValidated(
-        string hashkey,
-        address[] users,
-        uint256[] randomness,
-        uint32[] packIds
-    );
-    event Open(
-        string hashkey,
-        address user,
-        uint32 packId, 
-        uint32 qty
+        bytes32 hashkey,
+        uint32[] packIds,
+        uint256[] orderIds,
+        uint256[] randoms
     );
 
     struct Pack {
-        address token;
-
-        // uint32 for pack id
-        // uint256[] is an array representing the smaller value of chance for each prize
-        // e.g: Item 1 10% chance, value 0 and Item 2 90% chance, value 10 
-        // + the last element of the array represinting the last max value of last item(Item 1), 99
         uint256[] prizes;
-
         uint256[] prices;
-
-        uint256 price;
         bool editable;
     }
 
-    struct Session {
-        string hashkey;
-        uint32 remaining;
-    }
-
     mapping(uint256 => Pack) public packs;
-    mapping(address => mapping(uint32 => Session)) public userToSession;
     mapping(address => bool) public validators;
 
     address public houseAddress;
@@ -57,10 +32,6 @@ contract LootKingdom is Ownable {
         uint32 packId,
         Pack calldata pack
     ) external onlyOwner {
-        if (pack.prizes.length > 100 || pack.prizes.length != pack.prices.length + 1) {
-            revert InvalidLength();
-        }
-
         if (packs[packId].prizes.length != 0 && !packs[packId].editable) {
             revert NotEditable();
         }
@@ -80,60 +51,22 @@ contract LootKingdom is Ownable {
         packs[packId].editable = !packs[packId].editable;
     }
 
-    function setHashkey(uint32 packId, string calldata hashkey) external {
-        if (userToSession[msg.sender][packId].remaining != 0) {
-            revert CurrentlyUnchangeable();
-        }
-        userToSession[msg.sender][packId].hashkey = hashkey;
-    }
-
-    function open(
-        uint32 packId,
-        uint32 qty
-    ) external payable {
-        if (packs[packId].token == address(0)) {
-            if (msg.value < qty * packs[packId].price) {
-                revert InsufficientBalance();
-            }
-            payable(houseAddress).transfer(msg.value);
-        } else {
-            IERC20 token = IERC20(packs[packId].token);
-            uint256 balance = token.balanceOf(address(this));
-            if (balance < qty * packs[packId].price) {
-                revert InsufficientBalance();
-            }
-            token.transfer(houseAddress, balance);
-        }
-
-        userToSession[msg.sender][packId].remaining += qty;
-        emit Open(
-            userToSession[msg.sender][packId].hashkey,
-            msg.sender,
-            packId, 
-            qty
-        );
-    }
-
     function batchValidateOpens(
-        string calldata hashkey,
-        address[] calldata users,
-        uint32[] calldata packIds
-    ) external returns(uint256[] memory randValues){
+        bytes32 blockHash,
+        uint32[] calldata packIds,
+        uint256[] calldata orderIds
+    ) external {
         if (!validators[msg.sender]) {
             revert Forbidden();
         }
-        randValues = new uint256[](users.length);
-        for (uint256 i; i < users.length; ++i) {
-            randValues[i] = uint256(
-                keccak256(
-                    abi.encodePacked(
-                        userToSession[users[i]][packIds[i]].hashkey, hashkey
-                    )
-                )
-            ) % packs[packIds[i]].prizes[packs[packIds[i]].prizes.length-1];
-            --userToSession[users[i]][packIds[i]].remaining;
-        }
-        emit OpensValidated(hashkey, users, randValues, packIds);
-    }
 
+        uint256[] memory randValues = new uint256[](orderIds.length);
+        uint256 rand = uint256(blockHash);
+
+        for (uint256 i; i < orderIds.length; ++i) {
+            randValues[i] = rand % packs[packIds[i]].prizes[packs[packIds[i]].prizes.length-1];
+        }
+        
+        emit OpensValidated(blockHash, packIds, orderIds, randValues);
+    }
 }
