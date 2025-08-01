08037f70b60f8192014eb5c29abb24c8cb7a00c5
DIIMIIM <emilian@stater.co>
2025-08-01 19:31:12 +0300

    feat: working on contracts

--- contracts/UserKeys.sol
+++ contracts/UserKeys.sol
@@ -1,34 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import "./Validators.sol";
 import "./Ownable.sol";
 
 error Forbidden();
 
 contract UserKeys is Ownable {
-    event UserKeyUpdated(uint256 indexed userId, string updatedKey);
-    address public validatorAddress;
-    mapping(uint256 => string) public userIdToKey;
+    address public userKeysManager;
+    mapping(uint256 => bytes8) public userIdToKey;
     
     constructor(
-        address _validatorAddress
+        address _userKeysManager
     ) Ownable(msg.sender) {
-        validatorAddress = _validatorAddress;
+        userKeysManager = _userKeysManager;
+    }
+
+    function setConfig(
+        address _userKeysManager
+    )
+        external
+        onlyOwner
+    {
+        userKeysManager = _userKeysManager;
     }
 
     function setUserKeys(
         uint256[] calldata userIds,
-        string[] calldata updatedKeys
+        bytes8[] calldata updatedKeys
     )
         external 
     {
-        if (!Validators(validatorAddress).validators(msg.sender)) {
+        if (userKeysManager != msg.sender) {
             revert Forbidden();
         }
 
         for (uint256 i; i < userIds.length; ++i) {
             userIdToKey[userIds[i]] = updatedKeys[i];
-            emit UserKeyUpdated(userIds[i], updatedKeys[i]);
         }
+    }
+
+    function getRandomness(
+        string memory userKey,
+        string calldata serverKey
+    ) 
+        external 
+        view 
+        returns (uint256) 
+    {
+        return uint256(keccak256(abi.encodePacked(serverKey, userKey)));
     }
 }
--- contracts/Validators.sol
+++ contracts/Validators.sol
@@ -2,17 +2,38 @@
 pragma solidity ^0.8.19;
 import "./Ownable.sol";
 
+error Forbidden();
+
 contract Validators is Ownable {
+    address public validatorsManagerAddress;
     mapping(address => bool) public validators;
 
-    constructor() Ownable(msg.sender) {}
+    constructor(
+        address _validatorsManagerAddress
+    ) 
+        Ownable(msg.sender) 
+    {
+        validatorsManagerAddress = _validatorsManagerAddress;
+    }
+
+    function setConfig(
+        address _validatorsManagerAddress
+    )
+        external
+        onlyOwner
+    {
+        validatorsManagerAddress = _validatorsManagerAddress;
+    }
 
     function setWhitelist(
         address[] calldata proposedValidators
     ) 
         external 
-        onlyOwner 
     {
+        if (validatorsManagerAddress != msg.sender) {
+            revert Forbidden();
+        }
+
         for (uint i; i < proposedValidators.length; ++i) {
             validators[proposedValidators[i]] = !validators[proposedValidators[i]];
         }
